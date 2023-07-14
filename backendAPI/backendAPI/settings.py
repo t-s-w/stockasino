6db0139dffbcd21e02804615bf464bbdc650f814
t-s-w <tobby.satyarama@gmail.com>
2023-07-14 15:11:15 +0800

    connect to postgres db

--- backendAPI/backendAPI/settings.py
+++ backendAPI/backendAPI/settings.py
@@ -81,12 +81,15 @@ WSGI_APPLICATION = 'backendAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ['DBNAME'],
+    'USER': os.environ['PGUSER'],
+    'PASSWORD': os.environ['PGPASSWORD'],
+    'HOST': os.environ['HOST'],
+    'PORT': '5432',
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
