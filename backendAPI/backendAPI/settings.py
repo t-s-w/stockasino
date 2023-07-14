58a0d4ab70289cab946a882f62e607710c502dbb
t-s-w <tobby.satyarama@gmail.com>
2023-07-14 14:41:58 +0800

    add .env to backend

--- backendAPI/backendAPI/settings.py
+++ backendAPI/backendAPI/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+environ.Env()
+environ.Env.read_env()
+print(os.environ['SECRET'])
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
