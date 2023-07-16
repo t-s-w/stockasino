bb66685a4f81ab3f54bd9d83be4e513368856f84
t-s-w <tobby.satyarama@gmail.com>
2023-07-16 15:21:42 +0800

    add refresh view to backend

--- backendAPI/api/urls.py
+++ backendAPI/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     # path('test/', views.test, name='test')
     path('stocks/<slug:slug>', views.stockDetails),
-    path('auth/login',views.LoginTokenPairView.as_view())
+    path('auth/login',views.LoginTokenPairView.as_view()),
+    path('auth/refresh',TokenRefreshView.as_view())
 ]
