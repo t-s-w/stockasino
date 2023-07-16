ca6e1ec0e344cd9bf42a07dd97acdf73f880ee28
t-s-w <tobby.satyarama@gmail.com>
2023-07-16 15:36:46 +0800

    create login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
 
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   )
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return <form>
+        <fieldset>
+            <input name="username" type="text" />
+            <input name="password" type="password" />
+            <button>Log in!</button>
+        </fieldset>
+    </form>
+}
