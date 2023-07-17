e64bbfc4224e3de7d08479e92e1a6729739d7ed2
t-s-w <tobby.satyarama@gmail.com>
2023-07-17 14:45:18 +0800

    add formik signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import { AuthProvider } from './auth/AuthContext'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./auth/AuthContext";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-
-
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<HomePage />
-        } />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
