e6a13e723ad324c4b9222af90c553310167adf2c
t-s-w <tobby.satyarama@gmail.com>
2023-07-17 14:03:35 +0800

    add formik

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,8 @@ import AuthContext from "../auth/AuthContext"
 import { useContext } from "react"
 import PrivateRoute from "../auth/PrivateRoute";
 import { Link } from "react-router-dom";
+import sendRequest from "../utils/sendRequest";
+import { APIURL } from "../utils/constants";
 
 export default function HomePage() {
     const { user, logout } = useContext(AuthContext);
@@ -9,5 +11,6 @@ export default function HomePage() {
         <PrivateRoute />
         <h1>Hello, {user?.username}</h1>
         <Link to="" onClick={logout}>Log out</Link>
+        <button onClick={() => sendRequest(APIURL + "test")}>test</button>
     </>
 }
