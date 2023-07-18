969fc6a3a278976bb7044b8c84561e63fe603671
t-s-w <tobby.satyarama@gmail.com>
2023-07-18 14:32:12 +0800

    make login in navbar a dropdown

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,35 +1,39 @@
-import React, { useContext } from "react"
-import AuthContext from "../auth/AuthContext"
+import React, { useContext } from "react";
+import AuthContext from "../auth/AuthContext";
 import { LoginFormElement, LoginInfo } from "../utils/types";
 import { useNavigate } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
 
 export default function Login() {
-    const navigate = useNavigate()
-    const { login } = useContext(AuthContext)
+  const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
 
-    async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
-        evt.preventDefault();
-        const form = evt.target as LoginFormElement
-        const credentials = {
-            username: form.username.value,
-            password: form.password.value
-        } as LoginInfo
-        try {
-            await login(credentials)
-            navigate('/')
-        }
-        catch (err) {
-            if (err instanceof Error) {
-                console.log(err.message)
-            }
-        }
+  async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+    const form = evt.target as LoginFormElement;
+    const credentials = {
+      username: form.username.value,
+      password: form.password.value,
+    } as LoginInfo;
+    try {
+      await login(credentials);
+      navigate("/");
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      }
     }
+  }
 
-    return <form onSubmit={(evt) => void handleSubmit(evt)}>
+  return (
+    <NavDropdown title="Login">
+      <form onSubmit={(evt) => void handleSubmit(evt)}>
         <fieldset>
-            <input name="username" type="text" />
-            <input name="password" type="password" />
-            <button>Log in!</button>
+          <input name="username" type="text" />
+          <input name="password" type="password" />
+          <button>Log in!</button>
         </fieldset>
-    </form>
+      </form>
+    </NavDropdown>
+  );
 }
