9e6bc589be47998dcc6a25d00bfd6df1c387abe1
t-s-w <tobby.satyarama@gmail.com>
2023-07-20 09:54:37 +0800

    Adjust game info component to use react components

--- src/components/GameInfoButton.tsx
+++ src/components/GameInfoButton.tsx
@@ -1,7 +1,18 @@
 import { useContext } from "react";
 import AuthContext from "../auth/AuthContext";
+import { Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function GameInfoButton() {
+  const navigate = useNavigate();
   const { user } = useContext(AuthContext);
-  return user.game ? <></> : "Not currently playing a game. Start one!";
+  return user.game ? (
+    <></>
+  ) : (
+    <Nav.Item>
+      <Nav.Link onClick={() => navigate("/user/games")}>
+        No active game
+      </Nav.Link>
+    </Nav.Item>
+  );
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,14 +1,31 @@
 import AuthContext from "../auth/AuthContext";
 import { useContext } from "react";
 import GameInfoButton from "./GameInfoButton";
+import { Nav, NavDropdown } from "react-bootstrap";
 
 export default function UserCard() {
   const { user } = useContext(AuthContext);
 
   return (
-    <>
-      <span>Hi, {user.username}!</span>
+    <Nav variant="underline">
+      <NavDropdown
+        className="text-center"
+        title={
+          <span>
+            Logged in as
+            <br />
+            {user.username}
+          </span>
+        }
+        drop="down-centered"
+      >
+        test
+      </NavDropdown>
       <GameInfoButton />
-    </>
+    </Nav>
+    // <>
+    //   <span>Hi, {user.username}!</span>
+    //   <GameInfoButton />
+    // </>
   );
 }
